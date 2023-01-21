a8721cfd4b84b3c16f94c971c3574677d4f8edde
danieltvaz <danieltostes@live.com>
2023-01-21 00:56:17 -0300

    feat: v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useEffect, useState } from "react";
+
+import Button from "./components/button";
+import CustomSelect from "./components/custom-select";
+import Logo from "./components/logo";
+import MainWrapper from "./components/main-wrapper";
+import SectionContainer from "./components/section-container";
+import Spacer from "./components/spacer";
+import TextInput from "./components/text-input";
+import Typography from "./components/typography";
+//@ts-ignore
+import sound from "./assets/audio/alert.wav";
+import useCounter from "./hooks/useCounter";
+
+const alertSound = new Audio(sound);
+
+const DEFAULT_TIME = 60;
 
 function App() {
+  const { count, startCounter, clearCounter } = useCounter();
+  const [currentXp, setCurrentXp] = useState("");
+  const [targetXp, setTargetXp] = useState("");
+  const [result, setResult] = useState("");
+  const [unit, setUnit] = useState<"M" | "BI">("M");
+  const [customTime, setCustomTime] = useState("16");
+
+  function formatResult(value: string) {
+    const valueInNumber = Number(value);
+
+    if (unit === "BI") {
+      if (valueInNumber >= 1000) {
+        return `${valueInNumber / 1000} tri`;
+      }
+      if (valueInNumber < 1000) {
+        return `${valueInNumber} bi`;
+      }
+    }
+    if (unit === "M") {
+      if (valueInNumber < 1000) {
+        return `${valueInNumber} m`;
+      }
+      if (valueInNumber >= 1000 && valueInNumber < 100000) {
+        return `${valueInNumber / 1000} bi`;
+      }
+      if (valueInNumber >= 100000) {
+        return `${valueInNumber / 100000} tri`;
+      }
+    }
+
+    return result;
+  }
+
+  function calculateXpTimes() {
+    const perMinute = formatResult(result);
+    const perHour = formatResult(String(+result * 60));
+    const perCustomTime = formatResult(String(+result * 60 * +customTime));
+
+    return { perMinute, perHour, perCustomTime };
+  }
+
+  function calculateXpDiference() {
+    const currentXpWithoutMask = Number(currentXp.replace(/[\,\.]/g, ""));
+    const targetXpWithoutMask = Number(targetXp.replace(/[\,\.]/g, ""));
+
+    setResult((targetXpWithoutMask - currentXpWithoutMask).toString());
+  }
+
+  useEffect(() => {
+    if (!count) {
+      alertSound.play();
+    }
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainWrapper>
+      <Logo />
+      <SectionContainer direction="column">
+        <Typography>Digite seu XP atual e clique em começar contador</Typography>
+        <Spacer orientation="vertical" />
+        <SectionContainer direction="row">
+          <TextInput value={currentXp} onChange={(e) => setCurrentXp(e.target.value)} />
+          <Spacer orientation="horizontal" />
+          <CustomSelect
+            onChange={(e) => setUnit(e.currentTarget.value as "M" | "BI")}
+            options={[
+              { id: 1, value: "M", title: "M" },
+              { id: 2, value: "BI", title: "BI" },
+            ]}
+          />
+        </SectionContainer>
+        <Spacer orientation="vertical" />
+        <Typography>{count} segundos restantes</Typography>
+        <Spacer orientation="vertical" />
+        <Button onClick={startCounter} disabled={count === 0 || count !== DEFAULT_TIME}>
+          Começar contador
+        </Button>
+        <Spacer orientation="vertical" />
+        <Button onClick={clearCounter}>Resetar contador</Button>
+        <Spacer orientation="vertical" />
+        <Spacer orientation="vertical" />
+        <Typography>Digite seu XP no momento que o contador zerou</Typography>
+        <Spacer orientation="vertical" />
+        <TextInput value={targetXp} onChange={(e) => setTargetXp(e.target.value)} />
+        <Spacer orientation="vertical" />
+        <Button onClick={calculateXpDiference}>Calcular</Button>
+        <Spacer orientation="vertical" />
+        <Spacer orientation="vertical" />
+        {!!result && (
+          <>
+            <Typography>Você fez {calculateXpTimes().perMinute} de XP por minuto</Typography>
+            <Typography>Em uma hora faria {calculateXpTimes().perHour}</Typography>
+            <Typography>
+              Em <input className="hour-input" value={customTime} onChange={(e) => setCustomTime(e.target.value)} />{" "}
+              horas faria {calculateXpTimes().perCustomTime}
+            </Typography>
+          </>
+        )}
+      </SectionContainer>
+    </MainWrapper>
   );
 }
 
