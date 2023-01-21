117e8989922a6f1c521dd51dd3ba810c2ac9ee09
danieltvaz <danieltostes@live.com>
2023-01-21 00:55:07 -0300

    feat: logo comp

---
+++ src/components/logo/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function Logo() {
+  return (
+    <img
+      src="https://wartale.com/uploads/monthly_2019_06/65302963_429079371013460_6751369829724192768_n.png.8fe04941601cdbe254348972c43ff9c0.png"
+      alt="Wartale logo"
+      className="img"
+    />
+  );
+}
