7c2e4618f1177b6f13e0a873048b807d6123c8b6
danieltvaz <danieltostes@live.com>
2023-01-22 15:49:46 -0300

    feat: abstract numbers constant

--- src/constants/numbers.ts
+++ src/constants/numbers.ts
@@ -1,2 +1,9 @@
-export const TRI = 1000000000000;
-export const BI = 1000000000;
+const TRI = 1000000000000;
+const BI = 1000000000;
+const M = 1000000;
+
+export const NUMBERS = {
+  tri: TRI,
+  bi: BI,
+  m: M,
+};
