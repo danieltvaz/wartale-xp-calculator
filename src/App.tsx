31ffe8587e057711fccac040b40acd0c6e38157d
danieltvaz <danieltostes@live.com>
2024-02-11 15:45:20 -0300

    feat: add keep lock screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
 
 import Router from "./router";
+import keepAwake from "./services/keep-awake";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    keepAwake();
+  }, []);
+
   return <Router />;
 }
 
