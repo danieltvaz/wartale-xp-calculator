b6ac3956823d79865aa156e2c4e62dffe74b7a57
danieltvaz <danieltostes@live.com>
2024-02-13 17:53:12 -0300

    feat: add map and party size to history

--- src/components/history-add-modal/index.tsx
+++ src/components/history-add-modal/index.tsx
@@ -1,24 +1,35 @@
 import "./styles.css";
 
 import { Character, XPHistory } from "../../types";
-import { Dispatch, SetStateAction, useState } from "react";
+import CustomSelect, { CustomSelectProps } from "../custom-select";
+import { Dispatch, SetStateAction, useMemo, useState } from "react";
 import { formatResult, timeStampToDate } from "../../utils";
 
 import Button from "../button";
-import CustomSelect from "../custom-select";
 import Divider from "../divider";
 import Logo from "../logo";
+import { MAPS } from "../../constants/maps";
 import SectionContainer from "../section-container";
 import Spacer from "../spacer";
+import Typography from "../typography";
 import charactersHandler from "../../utils/characters-handler";
 
 type XPHistoryModalProps = {
   onAdd?: (history: Omit<XPHistory, "id">) => any | void;
   visible: boolean;
   setVisible: Dispatch<SetStateAction<boolean>>;
-  xpData: Omit<XPHistory, "id" | "characterId">;
+  xpData: Omit<XPHistory, "id" | "characterId" | "map" | "partySize">;
   defaultCharacter?: Character;
 };
+
+const PARTY_DATA = [
+  { id: 1, title: "Solo", value: 1 },
+  { id: 2, title: "2", value: 2 },
+  { id: 3, title: "3", value: 3 },
+  { id: 4, title: "4", value: 4 },
+  { id: 5, title: "5", value: 5 },
+  { id: 6, title: "6", value: 6 },
+] as CustomSelectProps["options"];
 
 export default function AddXPHistoryModal({
   onAdd,
@@ -29,52 +40,107 @@ export default function AddXPHistoryModal({
 }: XPHistoryModalProps) {
   const { getAllCharacters } = charactersHandler();
   const [character, setCharacter] = useState(0);
+  const [selectedMap, setSelectedMap] = useState(0);
+  const [selectedPartySize, setSelectPartySize] = useState(1);
 
   function handleOnAdd() {
-    onAdd?.({ ...xpData, characterId: character });
+    onAdd?.({ ...xpData, characterId: character, map: selectedMap, partySize: selectedPartySize });
     setVisible(false);
   }
 
-  function charactersToSelectFactory(characters: Character[]) {
-    const newCharacters = characters.map((character, index) => ({
+  const charactersToSelectFactory = useMemo(() => {
+    const newCharacters = getAllCharacters().map((character) => ({
       id: character.id,
       title: character.name,
       value: character.id,
     }));
     return newCharacters;
-  }
+  }, [getAllCharacters]);
+
+  const mapsToSelectFactory = useMemo(() => {
+    const newMaps = MAPS.map((map, index) => ({
+      id: index,
+      title: `${map.level || "0"} - ${map.name}`,
+      value: index,
+    }));
+    return newMaps;
+  }, []);
 
   return (
     <div className={`add-xp-history-modal--wrapper add-xp-history-modal--wrapper_${visible}`}>
       <div className="add-xp-history-modal-content--wrapper">
-        <SectionContainer>
+        <SectionContainer flex={1} justify="center" align="center">
           <Logo />
         </SectionContainer>
-        <SectionContainer direction="column">
-          <div>
-            <CustomSelect
-              options={charactersToSelectFactory(getAllCharacters())}
-              onChange={(e) => setCharacter(+e.currentTarget.value)}
-            />
-          </div>
-          <Divider />
-          <p>Data: {timeStampToDate(new Date().getTime())}</p>
-          <Divider />
-          <p>Nível atual: {xpData.currentLevel}</p>
-          <Divider />
-          <p>XP atual: {formatResult(xpData.currentXp)}</p>
-          <Divider />
-          <p>XP alvo: {formatResult(xpData.targetXp)}</p>
-          <Divider />
-          <p>XP/Hora: {formatResult(xpData.perHour)}</p>
-          <Divider />
-          <p>XP/Minuto: {formatResult(xpData.perMinute)}</p>
-          <Divider />
-          <p>
-            Próximo nível em: {xpData.nextLevelIn?.hours}H e {xpData.nextLevelIn?.minutes}M
-          </p>
+        <SectionContainer direction="column" justify="flex-start" flex={3}>
+          <SectionContainer>
+            <Typography color="#fff" textAlign="left">
+              Personagem:
+            </Typography>
+            <Spacer orientation="horizontal" />
+            <CustomSelect options={charactersToSelectFactory} onChange={(e) => setCharacter(+e.currentTarget.value)} />
+          </SectionContainer>
+          <Divider margin="8px" />
+          <SectionContainer>
+            <Typography color="#fff" textAlign="left">
+              Mapa:
+            </Typography>
+            <Spacer orientation="horizontal" />
+            <CustomSelect options={mapsToSelectFactory} onChange={(e) => setSelectedMap(+e.currentTarget.value)} />
+          </SectionContainer>
+          <Divider margin="8px" />
+          <SectionContainer>
+            <Typography color="#fff" textAlign="left">
+              Party:
+            </Typography>
+            <Spacer orientation="horizontal" />
+            <CustomSelect options={PARTY_DATA} onChange={(e) => setSelectPartySize(+e.currentTarget.value)} />
+          </SectionContainer>
+          <Divider margin="8px" />
+          <SectionContainer>
+            <Typography textAlign="left" color="#fff">
+              Data: {timeStampToDate(new Date().getTime())}
+            </Typography>
+          </SectionContainer>
+          <Divider margin="8px" />
+          <SectionContainer>
+            <Typography textAlign="left" color="#fff">
+              Nível atual: {xpData.currentLevel}
+            </Typography>
+          </SectionContainer>
+          <Divider margin="8px" />
+          <SectionContainer>
+            <Typography textAlign="left" color="#fff">
+              XP Início: {formatResult(xpData.currentXp)}
+            </Typography>
+          </SectionContainer>
+          <Divider margin="8px" />
+          <SectionContainer>
+            <Typography textAlign="left" color="#fff">
+              XP Fim: {formatResult(xpData.targetXp)}
+            </Typography>
+          </SectionContainer>
+          <Divider margin="8px" />
+          <SectionContainer>
+            <Typography textAlign="left" color="#fff">
+              XP/Hora: {formatResult(xpData.perHour)}
+            </Typography>
+          </SectionContainer>
+          <Divider margin="8px" />
+          <SectionContainer>
+            <Typography textAlign="left" color="#fff">
+              XP/Minuto: {formatResult(xpData.perMinute)}
+            </Typography>
+          </SectionContainer>
+          <Divider margin="8px" />
+          <SectionContainer>
+            <Typography textAlign="left" color="#fff">
+              {" "}
+              Próximo nível em: {xpData.nextLevelIn?.hours}H e {xpData.nextLevelIn?.minutes}M
+            </Typography>
+          </SectionContainer>
         </SectionContainer>
-        <SectionContainer direction="column">
+        <SectionContainer direction="column" justify="flex-end" flex={1} align="center">
           <Button onClick={handleOnAdd}>Salvar</Button>
           <Spacer orientation="vertical" />
           <Button onClick={() => setVisible(false)}>Cancelar</Button>
