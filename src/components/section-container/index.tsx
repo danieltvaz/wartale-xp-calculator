e5d770e48a20450ddf2f5f5655cc99f516edbea3
danieltvaz <danieltostes@live.com>
2024-02-13 17:53:32 -0300

    feat: add flex props

--- src/components/section-container/index.tsx
+++ src/components/section-container/index.tsx
@@ -5,11 +5,16 @@ import { ReactNode } from "react";
 type SectionContainerProps = {
   children: ReactNode;
   direction?: "row" | "column";
+  align?: "flex-start" | "center" | "flex-end";
+  justify?: "flex-start" | "center" | "flex-end" | "space-between" | "space-around" | "space-evenly";
+  flex?: number;
 };
 
-export default function SectionContainer({ children, direction }: SectionContainerProps) {
+export default function SectionContainer({ children, direction, align, justify, flex }: SectionContainerProps) {
   return (
-    <div className="sectioncontainer--wrapper" style={{ flexDirection: direction }}>
+    <div
+      className="sectioncontainer--wrapper"
+      style={{ flexDirection: direction, alignItems: align, justifyContent: justify, flex: flex }}>
       {children}
     </div>
   );
